5110ce279af1e4bdbe1731a49824454f4d700cfe
Remi Cattiau <remi@cattiau.com>
2019-08-22 08:32:32 -0700

    feature: add editing interface will be publish on our github page

---
+++ editor/src/App.js
@@ -0,0 +1,237 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Paper from "@material-ui/core/Paper";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import PublishIcon from "@material-ui/icons/Publish";
+import SearchIcon from "@material-ui/icons/Search";
+import CommitIcon from "mdi-material-ui/SourceCommitLocal";
+import SplitPane from "react-split-pane";
+import ReactMde from "react-mde";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import FileSelector from "./FileSelector";
+import Select from "react-select";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${
+  pdfjs.version
+}/pdf.worker.js`;
+
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2)
+  },
+  text: {
+    padding: theme.spacing(2, 2, 0)
+  },
+  paper: {
+    paddingBottom: 50
+  },
+  list: {
+    marginBottom: theme.spacing(2)
+  },
+  subheader: {
+    backgroundColor: theme.palette.background.paper
+  },
+  appBar: {
+    top: "auto",
+    bottom: 0
+  },
+  grow: {
+    flexGrow: 1
+  },
+  fabButtons: {
+    position: "absolute",
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: "0 auto"
+  }
+}));
+
+export default function App() {
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  const [mdocr, setMdocr] = React.useState();
+  const url = "http://localhost:18181";
+  const [value, setValue] = React.useState("");
+  const [pageNumber, setPageNumber] = React.useState(1);
+  const [current, setCurrent] = React.useState(null);
+  const [preview, setPreview] = React.useState(null);
+  const [numPages, setNumPages] = React.useState(0);
+  const [previewContent, setPreviewContent] = React.useState(null);
+  const [previewLoading, setPreviewLoading] = React.useState(false);
+
+  const [state, setState] = React.useState({
+    single: "",
+    popper: ""
+  });
+  const classes = useStyles();
+  const theme = useTheme();
+
+  if (!mdocr) {
+    (async () => {
+      let res = await fetch(`${url}/mdocr`);
+      setMdocr(await res.json());
+    })();
+    return (
+      <div>
+        <CircularProgress className={classes.progress} />
+      </div>
+    );
+  }
+
+  const handleChange = name => (event, { newValue }) => {
+    setState({
+      ...state,
+      [name]: newValue
+    });
+  };
+  const getBuildMarkdown = async () => {};
+
+  const getPreviewContent = async prev => {
+    setPreviewLoading(true);
+    let res = await fetch(`${url}/${prev.value}/${current.gitPath}`);
+    if (prev.value === "pdf") {
+      setPreviewContent(await res.arrayBuffer());
+    } else {
+      setPreviewContent(await res.text());
+    }
+    setPreviewLoading(false);
+  };
+  let previewElement = (
+    <SearchIcon style={{ width: "128px", height: "128px", color: "#999" }} />
+  );
+  //let addComponent = false;
+  if (previewLoading) {
+    //addComponent = true;
+    previewElement = <CircularProgress className={classes.progress} />;
+  } else if (preview) {
+    if (preview.value === "build") {
+      previewElement = <pre>{previewContent}</pre>;
+    } else if (preview.value === "html") {
+      previewElement = <div>{previewContent}</div>;
+    } else if (preview.value === "pdf") {
+      let pages = [];
+      for (let i = 1; i <= numPages; i++) {
+        pages.push(<Page id={i} pageNumber={i} />);
+      }
+      previewElement = (
+        <Document
+          file={previewContent}
+          onLoadSuccess={({ numPages }) => {
+            setNumPages(numPages);
+          }}
+        >
+          {pages}
+        </Document>
+      );
+    }
+  }
+  /*
+  let previewComponent = (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        width: "100%",
+        bottom: "60px",
+        top: "48px"
+      }}
+    >
+      {previewElement}
+    </div>
+  );*/
+
+  const getDraftMarkdown = async () => {};
+  return (
+    <SplitPane
+      split="vertical"
+      defaultSize={parseInt(
+        localStorage.getItem("splitPos") || window.innerWidth / 2,
+        10
+      )}
+      onChange={size => localStorage.setItem("splitPos", size)}
+    >
+      <div>
+        <ReactMde
+          value={value}
+          onChange={setValue}
+          minEditorHeight="inherit"
+          generateMarkdownPreview={async markdown => {
+            if (!current) {
+              return "";
+            }
+            let res = await fetch(`${url}/build/${current.gitPath}`);
+            return `<pre>${await res.text()}</pre>`;
+          }}
+        />
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
+          <Toolbar>
+            <FileSelector
+              drafts={Object.values(mdocr.files)}
+              onChange={async value => {
+                setCurrent(value);
+                let res = await fetch(`${url}/drafts/${value.gitPath}`);
+                setValue(await res.text());
+              }}
+            />
+            <IconButton color="inherit">
+              <AddIcon />
+            </IconButton>
+            <div className={classes.grow}>Repository: {mdocr.path}</div>
+            <IconButton color="inherit">
+              <CommitIcon />
+            </IconButton>
+            <IconButton edge="end" color="inherit">
+              <PublishIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div>
+        <div
+          style={{
+            backgroundColor: "#f9f9f9",
+            display: "flex",
+            height: "44px",
+            borderBottom: "1px solid #c8ccd0",
+            borderRadius: "2px 2px 0 0"
+          }}
+        >
+          <div style={{ padding: 10 }}>Preview</div>
+          <div style={{ paddingRight: 10, paddingTop: 3, flexGrow: 1 }}>
+            <Select
+              value={preview}
+              onChange={value => {
+                setPreview(value);
+                getPreviewContent(value);
+              }}
+              placeholder="Select a type of preview"
+              options={[
+                { label: "PDF", value: "pdf" },
+                { label: "HTML", value: "html" },
+                { label: "Markdown", value: "build" }
+              ]}
+            />
+          </div>
+          <div>
+            <IconButton color="inherit">
+              <RefreshIcon />
+            </IconButton>
+          </div>
+        </div>
+        {previewElement}
+      </div>
+    </SplitPane>
+  );
+}
---
+++ editor/src/FileSelector.js
@@ -0,0 +1,332 @@
+import { makeStyles, useTheme, emphasize } from "@material-ui/core/styles";
+import React from "react";
+import PropTypes from "prop-types";
+import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
+import Paper from "@material-ui/core/Paper";
+import Select from "react-select";
+import TextField from "@material-ui/core/TextField";
+import NoSsr from "@material-ui/core/NoSsr";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    minWidth: "50%"
+  },
+  input: {
+    display: "flex",
+    padding: 0,
+    height: "auto",
+    color: "white"
+  },
+  valueContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flex: 1,
+    alignItems: "center",
+    overflow: "hidden",
+    color: "white"
+  },
+  chip: {
+    margin: theme.spacing(0.5, 0.25)
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === "light"
+        ? theme.palette.grey[300]
+        : theme.palette.grey[700],
+      0.08
+    )
+  },
+  noOptionsMessage: {
+    padding: theme.spacing(1, 2)
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  placeholder: {
+    position: "absolute",
+    left: 2,
+    bottom: 6,
+    fontSize: 16,
+    color: "white"
+  },
+  paper: {
+    position: "absolute",
+    zIndex: 1,
+    marginTop: theme.spacing(1),
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  divider: {
+    height: theme.spacing(2)
+  }
+}));
+
+function NoOptionsMessage(props) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+NoOptionsMessage.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * Props to be passed on to the wrapper.
+   */
+  innerProps: PropTypes.object.isRequired,
+  selectProps: PropTypes.object.isRequired
+};
+
+function inputComponent({ inputRef, ...props }) {
+  return <div ref={inputRef} {...props} />;
+}
+
+inputComponent.propTypes = {
+  inputRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({
+      current: PropTypes.any.isRequired
+    })
+  ])
+};
+
+function Option(props) {
+  return (
+    <MenuItem
+      ref={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
+
+Option.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * props passed to the wrapping element for the group.
+   */
+  innerProps: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    key: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    onMouseMove: PropTypes.func.isRequired,
+    onMouseOver: PropTypes.func.isRequired,
+    tabIndex: PropTypes.number.isRequired
+  }).isRequired,
+  /**
+   * Inner ref to DOM Node
+   */
+  innerRef: PropTypes.oneOfType([
+    PropTypes.oneOf([null]),
+    PropTypes.func,
+    PropTypes.shape({
+      current: PropTypes.any.isRequired
+    })
+  ]).isRequired,
+  /**
+   * Whether the option is focused.
+   */
+  isFocused: PropTypes.bool.isRequired,
+  /**
+   * Whether the option is selected.
+   */
+  isSelected: PropTypes.bool.isRequired
+};
+
+function SingleValue(props) {
+  return (
+    <Typography
+      className={props.selectProps.classes.singleValue}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+SingleValue.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * Props passed to the wrapping element for the group.
+   */
+  innerProps: PropTypes.any.isRequired,
+  selectProps: PropTypes.object.isRequired
+};
+
+function Placeholder(props) {
+  const { selectProps, innerProps = {}, children } = props;
+  return (
+    <Typography
+      color="textSecondary"
+      className={selectProps.classes.placeholder}
+      {...innerProps}
+    >
+      {children}
+    </Typography>
+  );
+}
+
+Placeholder.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  /**
+   * props passed to the wrapping element for the group.
+   */
+  innerProps: PropTypes.object,
+  selectProps: PropTypes.object.isRequired
+};
+function ValueContainer(props) {
+  return (
+    <div className={props.selectProps.classes.valueContainer}>
+      {props.children}
+    </div>
+  );
+}
+
+ValueContainer.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.node,
+  selectProps: PropTypes.object.isRequired
+};
+
+function Menu(props) {
+  return (
+    <Paper
+      square
+      className={props.selectProps.classes.paper}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Paper>
+  );
+}
+
+Menu.propTypes = {
+  /**
+   * The children to be rendered.
+   */
+  children: PropTypes.element.isRequired,
+  /**
+   * Props to be passed to the menu wrapper.
+   */
+  innerProps: PropTypes.object.isRequired,
+  selectProps: PropTypes.object.isRequired
+};
+
+function Control(props) {
+  const {
+    children,
+    innerProps,
+    innerRef,
+    selectProps: { classes, TextFieldProps }
+  } = props;
+
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        classes: { imput: classes.input },
+        inputComponent,
+        inputProps: {
+          className: classes.input,
+          ref: innerRef,
+          children,
+          ...innerProps
+        }
+      }}
+      {...TextFieldProps}
+    />
+  );
+}
+
+const components = {
+  Control,
+  Menu,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer
+};
+
+export default function FileSelector(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const selectStyles = {
+    input: base => ({
+      ...base,
+      color: "white",
+      "& input": {
+        font: "inherit",
+        color: "white"
+      }
+    })
+  };
+
+  return (
+    <div className={classes.root}>
+      <NoSsr>
+        <Select
+          classes={classes}
+          styles={selectStyles}
+          inputId="react-select-single"
+          TextFieldProps={{
+            label: "File",
+            InputLabelProps: {
+              htmlFor: "react-select-single",
+              shrink: true,
+              style: { color: "white" }
+            }
+          }}
+          menuPlacement="top"
+          menuPosition="fixed"
+          components={components}
+          placeholder="Search for a file to edit"
+          options={props.drafts.map(i => ({
+            label: i.path,
+            value: i.path,
+            ...i
+          }))}
+          value={props.value}
+          onChange={(value, action) => {
+            if (props.onChange) {
+              props.onChange(value);
+            }
+          }}
+        />
+      </NoSsr>
+    </div>
+  );
+}
+
+/*
+
+
+        */
