1c2b4527675d7f693e0e12f3d446d36981ef921c
Remi Cattiau <remi@cattiau.com>
2020-11-24 22:49:21 -0800

    Updates

---
+++ precache-manifest.f35517a6adc5317d18696603058c97f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb9fe602f3316a3a1fc0bea2f93bc45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86975d8d8459fcd776b0",
+    "url": "/static/css/2.15c0f021.chunk.css"
+  },
+  {
+    "revision": "44242284b804b0c6c3e5",
+    "url": "/static/css/main.3c8aa44b.chunk.css"
+  },
+  {
+    "revision": "86975d8d8459fcd776b0",
+    "url": "/static/js/2.1088b364.chunk.js"
+  },
+  {
+    "revision": "44242284b804b0c6c3e5",
+    "url": "/static/js/main.ec87c19b.chunk.js"
+  },
+  {
+    "revision": "27cbe8c2c3f75c0c3b4a",
+    "url": "/static/js/runtime~main.20ca3034.js"
+  }
+]);
